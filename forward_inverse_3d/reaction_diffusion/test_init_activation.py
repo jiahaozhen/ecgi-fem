dd359b2d9e7a70e5c9e169bcb50efad7a5392fa0
jia <jiahaozhen@foxmail.com>
2025-12-09 17:13:30 +0800

    free wall activation

--- forward_inverse_3d/reaction_diffusion/test_init_activation.py
+++ forward_inverse_3d/reaction_diffusion/test_init_activation.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     case_name = case_name_list[0]
     mesh_file = f'forward_inverse_3d/data/mesh/mesh_{case_name}.msh'
 
-    activation_dict = get_activation_dict(case_name, mode='IVS', threshold=60)
+    activation_dict = get_activation_dict(case_name, mode='FREEWALL')
 
     target_pts = np.asarray(list(activation_dict.values()))
 
-    scatter_on_mesh(mesh_file, target_pts, target_cell=2)
+    scatter_on_mesh(mesh_file, target_pts, target_cell=2, title='init activation')
